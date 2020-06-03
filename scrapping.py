514e52317372cbd4cebabeae7e1bb0c2b05ad69b
DavidCarricondo <dacsmail00@gmail.com>
2020-06-03 13:39:36 +0200

    Search bar

--- scrapping.py
+++ scrapping.py
@@ -1,4 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 #https://sites.google.com/a/chromium.org/chromedriver/downloads
 PATH = '/home/david/Chrome_web_driver/chromedriver'
@@ -6,4 +11,24 @@ driver = webdriver.Chrome(PATH)
 
 driver.get('https://techwithtim.net')
 print(driver.title)
-driver.quit()
+
+search = driver.find_element_by_name('s')    
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+#Wait until the key has been found:
+try:
+    main = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+    articles = main.find_elements_by_tag_name("article")
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+finally:
+    driver.quit()
+
+
+
+time.sleep(5)
+driver.quit()   
