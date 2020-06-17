9379b728cad089ff74964424fbda7b07c3a16acf
DavidCarricondo <dacsmail00@gmail.com>
2020-06-17 19:58:54 +0200

    get into the book page and look for reviews

--- src/goodreads_scrapping.py
+++ src/goodreads_scrapping.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
 
 dotenv.load_dotenv()
 
@@ -30,10 +32,43 @@ driver.implicitly_wait(1)
 password_field.send_keys(GR_PASS)
 driver.implicitly_wait(1)
 
-#Click the submit button
+#Click the submit button (the third element formbox)
 submit = driver.find_elements_by_class_name('formBox')
 submit[2].click()
 
 driver.implicitly_wait(5) 
+
+
+###FIND A BOOK
+
+BOOK_NAME = input('Give me a book to look for: ')
+
+inputs = driver.find_elements_by_tag_name('input')
+
+#The search box is the first input tag
+search_box = 0
+for i in inputs:
+    search_box = i
+    break
+
+#Input the book name and hit enter
+search_box.send_keys(BOOK_NAME)
+search_box.send_keys(Keys.RETURN)
+driver.implicitly_wait(5)
+
+#SELECT THE FIRST INSTANCE
+
+driver.find_element_by_class_name('bookTitle').click()
+
+#GET THE REVIEWS
+
+reviews_container = driver.find_elements_by_class_name('review')
+
+reviews = {}
+
+for i, e in enumerate(reviews_container):
+    reviews[i] = e.getText()
+
+print(reviews)
 
 #driver.quit()
