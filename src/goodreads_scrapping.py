bad97a1d661055dba43b0cac46385e5f6ffbd228
DavidCarricondo <dacsmail00@gmail.com>
2020-06-19 18:02:16 +0200

    Scrapping finished

--- src/goodreads_scrapping.py
+++ src/goodreads_scrapping.py
@@ -1,9 +1,7 @@
 import os
 import dotenv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import json
 from goodread_utils import *
 
